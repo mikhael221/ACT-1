6c28c1c7859bd73650f321ff92143bd3f71ddebe
“mikhael-delossantos” <mikhaelacds@gmail.com>
2025-06-14 05:22:34 +0800

    push

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace ACT_1
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            string ageInput = Console.ReadLine();
+
+            int age;
+            if (int.TryParse(ageInput, out age))
+            {
+                Console.WriteLine($"\nHello, {name}, your current age is {age}!");
+
+                int plus = 5;
+                int future = age + plus;
+
+                Console.WriteLine($"\nHello, {name}, your age in the next 5 years will be {future}!");
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid age input. Please enter a number.");
+            }
         }
     }
 }
